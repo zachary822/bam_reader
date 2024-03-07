d57935f7072ba8ede058e963803d6ce10567490e
Zachary Juang <zachary822@me.com>
2024-03-07 01:25:47 -0500

    fix build

--- build.py
+++ build.py
@@ -1,13 +1,48 @@
-from Cython.Build import cythonize, build_ext
+import os
+import shutil
+
+from Cython.Build import build_ext, cythonize
+from setuptools import Distribution, Extension
+
+compile_args: list[str] = ["-O3"]
+link_args: list[str] = []
+include_dirs: list[str] = []
+libraries: list[str] = []
 
 
-def build(setup_kwargs):
-    setup_kwargs.update(
-        {
-            "ext_modules": cythonize(
-                "bam_reader/*.pyx",
-                language_level=3,
-            ),
-            "cmdclass": {"build_ext": build_ext},
-        }
+def build():
+    extensions = [
+        Extension(
+            "*",
+            ["bam_reader/*.pyx"],
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            include_dirs=include_dirs,
+            libraries=libraries,
+        )
+    ]
+    ext_modules = cythonize(
+        extensions,
+        include_path=include_dirs,
+        compiler_directives={"binding": True, "language_level": 3},
     )
+
+    distribution = Distribution({"name": "extended", "ext_modules": ext_modules})
+    distribution.package_dir = "extended"
+
+    cmd = build_ext(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        print(relative_extension)
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
