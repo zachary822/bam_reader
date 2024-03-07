db02ab4b23c624cfcd2fb8c2c3917f08689c0ec8
Zachary Juang <zachary822@me.com>
2024-03-07 00:53:17 -0500

    better implementation

--- bam_reader/bin2code.pyx
+++ bam_reader/bin2code.pyx
@@ -1,13 +1,16 @@
-from libc.stdlib cimport malloc
+from libc.stdlib cimport malloc, free
 
 CODE = b"=ACMGRSVTWYHKDBN"
 
-def bin2code(unsigned char[] bin):
-    cdef int i
-    cdef unsigned char * result = <unsigned char *> malloc(len(bin) * 2 * sizeof(unsigned char))
+def bin2code(unsigned char[] bin, int size):
+    cdef int i, b_len = len(bin)
+    cdef unsigned char * result = <unsigned char *> malloc(size * sizeof(unsigned char))
      
-    for i in range(len(bin)):
+    for i in range(b_len):
         result[i * 2] = CODE[bin[i] >> 4]
         result[i * 2 + 1] = CODE[bin[i] & 0b1111]
 
-    return result
+    try:
+        return result[:size]
+    finally:
+        free(result)
