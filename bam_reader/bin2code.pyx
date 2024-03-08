6d85f3b8c8bf7e8c506c3a938aa69665d7c763ff
Zachary Juang <zachary822@me.com>
2024-03-08 03:05:17 -0500

    better utils

--- bam_reader/bin2code.pyx
+++ bam_reader/bin2code.pyx
@@ -2,9 +2,9 @@ from libc.stdlib cimport malloc, free
 
 CODE = b"=ACMGRSVTWYHKDBN"
 
-def bin2code(unsigned char[] bin, int size):
+def bin2code(unsigned char *bin, int size):
     cdef int i, b_len = len(bin)
-    cdef unsigned char * result = <unsigned char *> malloc(size * sizeof(unsigned char))
+    cdef unsigned char * result = <unsigned char *> malloc(b_len * 2 * sizeof(unsigned char))
      
     for i in range(b_len):
         result[i * 2] = CODE[bin[i] >> 4]
---
+++ bam_reader/utils.py
@@ -0,0 +1,5 @@
+REV_COM = bytes.maketrans(b"AGCTYRWSKMDVHB", b"TCGARYWSMKHBDV")
+
+
+def reverse_complement(code: bytes) -> bytes:
+    return code.translate(REV_COM)
