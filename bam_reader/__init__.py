1991ee526ebd2d06706c70f8524c874674d8f6a0
Zachary Juang <zachary822@me.com>
2024-03-09 05:43:18 -0500

    better error handling

--- bam_reader/__init__.py
+++ bam_reader/__init__.py
@@ -20,79 +20,85 @@ class BzgfError(BamError):
 def decompress_bzgf(f: BinaryIO) -> bytearray:
     data = bytearray()
 
-    while header := f.read(12):
-        id1, id2, cm, flg, mtime, xfl, os_, xlen = struct.unpack("4BI2BH", header)
+    try:
+        while header := f.read(12):
+            id1, id2, cm, flg, mtime, xfl, os_, xlen = struct.unpack("4BI2BH", header)
 
-        if not (flg & FLG_FEXTRA):
-            raise BzgfError("gzip flag incorrectly set")
+            if not (flg & FLG_FEXTRA):
+                raise BzgfError("gzip flag incorrectly set")
 
-        end = f.tell() + xlen
+            end = f.tell() + xlen
 
-        bsize = 0
-        while f.tell() < end:
-            si1, si2, slen = struct.unpack("2BH", f.read(4))
+            bsize = 0
+            while f.tell() < end:
+                si1, si2, slen = struct.unpack("2BH", f.read(4))
 
-            sdata = f.read(slen)
-            if si1 == 66 and si2 == 67:
-                (bsize,) = struct.unpack("H", sdata)
-                break
-        else:
-            raise BzgfError("block size not found")
+                sdata = f.read(slen)
+                if si1 == 66 and si2 == 67:
+                    (bsize,) = struct.unpack("H", sdata)
+                    break
+            else:
+                raise BzgfError("block size not found")
 
-        f.seek(end)
-        cdata = f.read(bsize - xlen - 19)
+            f.seek(end)
+            cdata = f.read(bsize - xlen - 19)
 
-        infl = zlib.decompress(cdata, wbits=-zlib.MAX_WBITS)
+            infl = zlib.decompress(cdata, wbits=-zlib.MAX_WBITS)
 
-        crc_infl = zlib.crc32(infl)
+            crc_infl = zlib.crc32(infl)
 
-        (crc32, isize) = struct.unpack("2I", f.read(8))
+            (crc32, isize) = struct.unpack("2I", f.read(8))
 
-        if crc_infl != crc32:
-            raise BzgfError("crc32 does not match")
+            if crc_infl != crc32:
+                raise BzgfError("crc32 does not match")
 
-        if isize != len(infl):
-            raise BzgfError("decompressed size does not match")
+            if isize != len(infl):
+                raise BzgfError("decompressed size does not match")
 
-        data.extend(infl)
+            data.extend(infl)
 
-    return data
+        return data
+    except struct.error as e:
+        raise BzgfError(e.args[0]) from e
 
 
 def extract_sequence(bam: BinaryIO) -> list[bytes]:
-    magic, l_text = struct.unpack("<4sI", bam.read(8))
+    try:
+        magic, l_text = struct.unpack("<4sI", bam.read(8))
 
-    if magic != b"BAM\x01":
-        raise BamError("Incorrect magic")
+        if magic != b"BAM\x01":
+            raise BamError("Incorrect magic")
 
-    bam.seek(l_text, 1)
+        bam.seek(l_text, 1)
 
-    (n_ref,) = struct.unpack("<I", bam.read(4))
+        (n_ref,) = struct.unpack("<I", bam.read(4))
 
-    for i in range(n_ref):
-        (l_name,) = struct.unpack("<I", bam.read(4))
-        bam.seek(l_name + 4, 1)
+        for i in range(n_ref):
+            (l_name,) = struct.unpack("<I", bam.read(4))
+            bam.seek(l_name + 4, 1)
 
-    while header := bam.read(36):
-        (
-            block_size,
-            ref_id,
-            pos,
-            l_read_name,
-            mapq,
-            bin_,
-            n_cigar_op,
-            flag,
-            l_seq,
-            next_ref_id,
-            next_pos,
-            tlen,
-        ) = struct.unpack("<I2i2B3HI3i", header)
+        while header := bam.read(36):
+            (
+                block_size,
+                ref_id,
+                pos,
+                l_read_name,
+                mapq,
+                bin_,
+                n_cigar_op,
+                flag,
+                l_seq,
+                next_ref_id,
+                next_pos,
+                tlen,
+            ) = struct.unpack("<I2i2B3HI3i", header)
 
-        end = bam.tell() + block_size - 32
+            end = bam.tell() + block_size - 32
 
-        bam.seek(l_read_name + 4 * n_cigar_op, 1)
+            bam.seek(l_read_name + 4 * n_cigar_op, 1)
 
-        yield bin2code(bam.read((l_seq + 1) // 2), l_seq)
+            yield bin2code(bam.read((l_seq + 1) // 2), l_seq)
 
-        bam.seek(end)
+            bam.seek(end)
+    except struct.error as e:
+        raise BzgfError(e.args[0]) from e
