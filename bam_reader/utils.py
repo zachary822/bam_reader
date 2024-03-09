70c71062c90a815d33d0388429a0679e356425b8
Zachary Juang <zachary822@me.com>
2024-03-09 02:37:13 -0500

    fix reverse complement

--- bam_reader/utils.py
+++ bam_reader/utils.py
@@ -2,4 +2,4 @@ REV_COM = bytes.maketrans(b"AGCTYRWSKMDVHB", b"TCGARYWSMKHBDV")
 
 
 def reverse_complement(code: bytes) -> bytes:
-    return code.translate(REV_COM)
+    return code.translate(REV_COM)[::-1]
